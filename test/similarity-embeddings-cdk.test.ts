9adc3be474281e89ca70a712c7331febc9a23b20
order&chaos <sean@orderandchaoscreative.com>
2022-12-13 19:06:31 +0000

    Initial commit

---
+++ test/similarity-embeddings-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SimilarityEmbeddingsCdk from '../lib/similarity-embeddings-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/similarity-embeddings-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SimilarityEmbeddingsCdk.SimilarityEmbeddingsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
