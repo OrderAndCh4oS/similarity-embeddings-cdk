fe644dd746434c7c1b47916563fe2194d77d3171
order&chaos <sean@orderandchaoscreative.com>
2022-12-13 23:23:12 +0000

    efs + lambda set up

---
+++ lambdas/src/create_embedding/index.py
@@ -0,0 +1,17 @@
+import os
+import json
+from pathlib import Path
+
+def handler(event, context):
+    # Writing to a file on the EFS filesystem
+    path = Path("/mnt/filesystem/test.txt")
+    with path.open("w") as f:
+        f.write("Test123")
+    # Now open the file, read the text, return
+    with path.open("r") as f:
+        text = f.read()
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"message": f"Hello Lambda! {text}"})
+    }
--- lib/similarity-embeddings-cdk-stack.ts
+++ lib/similarity-embeddings-cdk-stack.ts
@@ -1,16 +1,67 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as efs from 'aws-cdk-lib/aws-efs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import {ThroughputMode} from 'aws-cdk-lib/aws-efs';
+import {Construct} from 'constructs';
+import {Architecture} from "aws-cdk-lib/aws-lambda";
+import {Duration, RemovalPolicy} from "aws-cdk-lib";
+import * as path from "path";
 
 export class SimilarityEmbeddingsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      maxAzs: 2
+    });
 
-    // The code that defines your stack goes here
+    const efsSecurityGroup = new ec2.SecurityGroup(this, 'EfsSimilarityEmbeddingSecurityGroup', {
+      vpc
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SimilarityEmbeddingsCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const fs = new efs.FileSystem(this, 'EfsSimilarityEmbeddingFileSystem', {
+      vpc: vpc,
+      performanceMode: efs.PerformanceMode.GENERAL_PURPOSE,
+      throughputMode: ThroughputMode.BURSTING,
+      removalPolicy: RemovalPolicy.DESTROY,
+      securityGroup: efsSecurityGroup
+    });
+
+    const accessPoint = fs.addAccessPoint('LambdaAccessPoint', {
+      path: '/export/lambda',
+      createAcl: {
+        ownerGid: '1001',
+        ownerUid: '1001',
+        permissions: '755',
+      },
+      posixUser: {
+        uid: '1001',
+        gid: '1001',
+      }
+    });
+
+    const lambdaSecurityGroup = new ec2.SecurityGroup(this, 'SimilarityEmbeddingLambdaSecurityGroup', {
+      vpc
+    });
+
+    const createEmbeddingHandler = new lambda.DockerImageFunction(this, 'CreateEmbeddingLambdaFunction', {
+      code: lambda.DockerImageCode.fromImageAsset(
+          path.join(__dirname, '../lambdas'),
+          {
+            cmd: ["create_embedding.index.handler"]
+          }
+      ),
+      memorySize: 1024,
+      timeout: Duration.seconds(10),
+      architecture: Architecture.ARM_64,
+      securityGroups: [lambdaSecurityGroup],
+      filesystem: lambda.FileSystem.fromEfsAccessPoint(accessPoint, '/mnt/filesystem'),
+      vpc,
+      // Todo: could store to the redis cache on creation…
+      // environment: {
+      //   CACHE_HOST: redisCache.attrRedisEndpointAddress,
+      //   CACHE_PORT: redisCache.attrRedisEndpointPort
+      // },
+    });
   }
 }
